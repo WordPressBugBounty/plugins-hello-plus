2a22a72a87332e4ae597ba9a21e7fccf3dfa9808
Sean Murphy <sean@iamseanmurphy.com>
2025-01-30 11:17:16 +0000

    1.1.3

---
+++ hello-plus/build/js/helloplus-forms-fe.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'be1eedae161e8c53bc38');
--- hello-plus/build/js/helloplus-onboarding.asset.php
+++ hello-plus/build/js/helloplus-onboarding.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => '4fad3bfbdfe156c510c8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => '2c8b85819ec933f317ec');
